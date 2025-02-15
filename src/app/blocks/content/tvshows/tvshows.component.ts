ad8f41f9f3eb36cb7c79ea0b2ba7a34a31d6366f
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-02-15 21:27:54 +0100

    15225

--- src/app/blocks/content/tvshows/tvshows.component.ts
+++ src/app/blocks/content/tvshows/tvshows.component.ts
@@ -12,42 +12,6 @@ export class TvshowsComponent {
   TMDB_URL = 'https://api.themoviedb.org/3';
   IMG_TMDB_URL = 'https://image.tmdb.org/t/p/w500';
 
-  categories = [
-    {
-      title: 'Trending Movies',
-      endpoint: '/trending/movie/week',
-      mediaType: 'movie',
-    },
-    {
-      title: 'Trending TV Shows',
-      endpoint: '/trending/tv/week',
-      mediaType: 'tv',
-    },
-    { title: 'In Cinema', endpoint: '/movie/upcoming', mediaType: 'movie' },
-    {
-      title: 'Popular Movies',
-      endpoint: '/movie/popular',
-      mediaType: 'movie',
-    },
-    { title: 'Popular TV Shows', endpoint: '/tv/popular', mediaType: 'tv' },
-    {
-      title: 'Top Rated Movies',
-      endpoint: '/movie/top_rated',
-      mediaType: 'movie',
-    },
-    {
-      title: 'Top Rated TV Shows',
-      endpoint: '/tv/top_rated',
-      mediaType: 'tv',
-    },
-    {
-      title: 'Now Playing',
-      endpoint: '/movie/now_playing',
-      mediaType: 'movie',
-    },
-    { title: 'On TV', endpoint: '/tv/on_the_air', mediaType: 'tv' },
-  ];
-
   constructor(private http: HttpClient) {
     this.callAPI(this.TMDB_URL, '/movie/upcoming', 'movie');
   }
