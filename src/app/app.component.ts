24b98f6e6c37ea00f08b927b4d817c97766ac881
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-02-16 22:06:20 +0100

    25216!
    huge bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './blocks/header/header.component';
   selector: 'app-root',
   imports: [RouterOutlet, SideBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'capi';
