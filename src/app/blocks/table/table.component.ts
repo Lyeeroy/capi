f88bb29a546efee9a667f8f5e5c5031edbffc9e1
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-02-19 17:51:13 +0100

    25219

--- src/app/blocks/table/table.component.ts
+++ src/app/blocks/table/table.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table',
-  imports: [],
   templateUrl: './table.component.html',
+  imports: [CommonModule],
 })
-export class TableComponent {}
+export class TableComponent {
+  sources: number[] = []; // Array to store sources
+
+  addSource() {
+    this.sources.unshift(this.sources.length + 1); // Insert at the beginning
+  }
+  trackByFn(index: number, item: number) {
+    return item; // Helps Angular track items correctly
+  }
+
+  isModalVisible = false;
+  isAdding = true;
+  openOrClose: 'open' | 'close' = 'close';
+
+  openModal(openOrClose: any) {
+    console.log('openModal fired. var: ', this.openOrClose);
+    if (this.openOrClose === 'close') {
+      this.openOrClose = 'open';
+      this.isModalVisible = true;
+    } else if (this.openOrClose === 'open') {
+      this.openOrClose = 'close';
+      this.isModalVisible = false;
+    }
+  }
+}
