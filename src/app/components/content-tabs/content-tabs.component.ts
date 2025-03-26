bb54427e5e4373c2b33f0ea3e043863bcc1bf197
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-03-26 21:43:43 +0100

    25326

--- src/app/components/content-tabs/content-tabs.component.ts
+++ src/app/components/content-tabs/content-tabs.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-  OnDestroy,
-} from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { TmdbService } from '../../services/tmdb.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
@@ -16,7 +10,7 @@ import { Subscription, forkJoin } from 'rxjs';
   imports: [CommonModule, RouterModule],
   standalone: true,
 })
-export class ContentTabsComponent implements OnChanges, OnDestroy {
+export class ContentTabsComponent implements OnInit, OnDestroy {
   @Input() trending: any[] = [];
   @Input() type: 'movie' | 'tv' = 'movie';
   @Input() apiEndpoint?: string;
@@ -33,44 +27,39 @@ export class ContentTabsComponent implements OnChanges, OnDestroy {
 
   constructor(private tmdbService: TmdbService, private router: Router) {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['apiEndpoint'] && changes['apiEndpoint'].currentValue) {
-      console.log('apiEndpoint changed:', changes['apiEndpoint'].currentValue); // Debugging
-      this.fetchData();
-    }
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
   }
 
   private fetchData(): void {
-    if (!this.apiEndpoint) {
-      return;
-    }
-
     this.subscription?.unsubscribe();
-    const endpoint = this.apiEndpoint;
+    const endpoint = this.apiEndpoint || `/discover/movie`; // Default to movies
     const params: { with_genres?: number; sort_by?: string } = {};
     if (this.genreId !== 0) params.with_genres = this.genreId;
     if (this.sortBy) params.sort_by = this.sortBy;
+
     const itemsPerPage = 20;
     const pagesNeeded = Math.max(1, Math.ceil(this._tileLimit / itemsPerPage));
     const requests = Array.from({ length: pagesNeeded }, (_, i) =>
       this.tmdbService.fetchFromTmdb(endpoint, { ...params, page: i + 1 })
     );
+
     this.subscription = forkJoin(requests).subscribe((pagesData) => {
       const results = pagesData.reduce(
         (acc, page) => acc.concat(page.results),
         []
       );
+
+      // Automatically determine media type if available
       this.trending = results.slice(0, this._tileLimit);
     });
   }
-
   redirectToPlayer(index: number): void {
     const selectedItem = this.trending[index];
-    this.router.navigate(['/player', selectedItem.id, this.type]);
+    const mediaType = selectedItem.media_type || 'movie'; // Default to 'movie'
+    this.router.navigate(['/player', selectedItem.id, mediaType]);
   }
 
   trackByFn(index: number, item: any): number {
