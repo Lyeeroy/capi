15e02b530a42f3625701ed78f94ca3d3e5a0c4ed
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-03-29 21:42:29 +0100

    25329

--- src/app/blocks/player/player.component.ts
+++ src/app/blocks/player/player.component.ts
@@ -14,6 +14,7 @@ import { PlaylistComponent } from './playlist/playlist.component';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
+
   standalone: true,
   imports: [
     CommonModule,
