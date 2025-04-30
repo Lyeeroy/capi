a0ec06d91a85d3e1932eba15c07b2de64471deb5
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-04-30 17:40:38 +0200

    25430

--- src/app/blocks/player/player-header/player-header.component.ts
+++ src/app/blocks/player/player-header/player-header.component.ts
@@ -5,15 +5,41 @@ import { IconLibComponent } from '../../../svg-icons/icon-lib.component';
   selector: 'app-player-header',
   standalone: true,
   template: `
-    <div class="flex items-center p-2">
-      <button (click)="onGoBack()" class="cursor-pointer"><- Go back</button>
-      <span class="text-gray-900 text-sm px-1"> / </span>
-      <h2 class=" text-gray-900 truncate px-1">
-        {{ responseData?.name || responseData?.title || 'Loading...' }}
-      </h2>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center py-2">
+        <button
+          (click)="onGoBack()"
+          class="flex items-center cursor-pointer text-sm text-gray-600 ml-2 hover:text-gray-800 active:text-gray-900 whitespace-nowrap"
+        >
+          <app-icon-lib ico="arrowLeft" class="w-4 h-4 mr-2"></app-icon-lib>
+          Go back
+        </button>
+        <span class="text-sm text-gray-400 px-2">/</span>
+        <h2
+          class="text-sm font-semibold text-gray-600 truncate hover:text-gray-800 active:text-gray-900"
+        >
+          {{ responseData?.name || responseData?.title || 'Loading...' }}
+        </h2>
+      </div>
+
+      <div class="hidden flex items-center gap-2">
+        <button
+          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer"
+        >
+          <app-icon-lib ico="play" class="w-4 h-4 mr-2"></app-icon-lib>
+          Playlist
+        </button>
+        <span class="text-sm text-gray-400">/</span>
+        <button
+          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer"
+        >
+          <app-icon-lib ico="menu" class="w-4 h-4 mr-2"></app-icon-lib>
+          Details
+        </button>
+      </div>
     </div>
   `,
-  imports: [IconLibComponent],
+  imports: [IconLibComponent, IconLibComponent],
 })
 export class PlayerHeader implements OnInit {
   constructor() {}
@@ -24,7 +50,9 @@ export class PlayerHeader implements OnInit {
   currentSeason: number = 1;
   names: string = '"Show Name"';
 
-  onGoBack() {}
+  onGoBack() {
+    window.history.back();
+  }
 
   ngOnInit() {}
 }
