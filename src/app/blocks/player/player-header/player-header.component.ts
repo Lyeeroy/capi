d6766fd35f4301ed30a209053cb7f37c31279754
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2025-05-02 14:42:39 +0200

    2552

--- src/app/blocks/player/player-header/player-header.component.ts
+++ src/app/blocks/player/player-header/player-header.component.ts
@@ -5,33 +5,64 @@ import { IconLibComponent } from '../../../svg-icons/icon-lib.component';
   selector: 'app-player-header',
   standalone: true,
   template: `
-    <div class="flex items-center justify-between">
-      <div class="flex items-center py-2">
-        <button
-          (click)="onGoBack()"
-          class="flex items-center cursor-pointer text-sm text-gray-600 ml-2 hover:text-gray-800 active:text-gray-900 whitespace-nowrap"
+    <div
+      class="py-2 flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center gap-2 md:gap-0"
+    >
+      <nav
+        class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-xl bg-gray-50 w-fit max-w-full"
+        aria-label="Breadcrumb"
+      >
+        <ol
+          class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse min-w-0"
         >
-          <app-icon-lib ico="arrowLeft" class="w-4 h-4 mr-2"></app-icon-lib>
-          Go back
-        </button>
-        <span class="text-sm text-gray-400 px-2">/</span>
-        <h2
-          class="text-sm font-semibold text-gray-600 truncate hover:text-gray-800 active:text-gray-900"
-        >
-          {{ responseData?.name || responseData?.title || 'Loading...' }}
-        </h2>
-      </div>
+          <li class="inline-flex items-center flex-shrink-0">
+            <button
+              (click)="onGoBack()"
+              class="inline-flex items-center text-sm font-medium text-gray-600 hover:text-blue-600 cursor-pointer whitespace-nowrap"
+            >
+              Go back
+            </button>
+          </li>
 
-      <div class="hidden flex items-center gap-2">
+          <li aria-current="page" class="min-w-0">
+            <div class="flex items-center">
+              <svg
+                class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 flex-shrink-0"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span
+                class="block ms-1 text-sm text-gray-600 md:ms-2 font-semibold whitespace-nowrap overflow-hidden text-ellipsis"
+              >
+                {{ responseData?.name || responseData?.title || 'Loading...' }}
+              </span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+
+      <div
+        class="hidden flex px-5 py-3 gap-2 text-gray-700 border border-gray-200 rounded-xl bg-gray-50 w-fit max-w-full flex-shrink-0"
+      >
         <button
-          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer"
+          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer whitespace-nowrap"
         >
           <app-icon-lib ico="play" class="w-4 h-4 mr-2"></app-icon-lib>
           Playlist
         </button>
-        <span class="text-sm text-gray-400">/</span>
+        <span class="text-sm text-gray-400 hidden sm:block">/</span>
         <button
-          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer"
+          class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 active:text-gray-900 cursor-pointer whitespace-nowrap"
         >
           <app-icon-lib ico="menu" class="w-4 h-4 mr-2"></app-icon-lib>
           Details
